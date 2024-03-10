7041b6457db760ce28b6a6640fc50a38ab925798
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-10 23:11:37 +0200

    changes path /(name of list) shows view for that list and adds new todolist

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("Hello")
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    return HttpResponse("<h1>%s</h1>" % ls.name)
 
-def v1(response):
-    return HttpResponse("view 1")
