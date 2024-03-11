7bab56cd312d72461404012feb63798e11f14940
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-11 05:18:02 +0200

    adds sidenav and static css folder with style for base

--- main/views.py
+++ main/views.py
@@ -8,13 +8,14 @@ def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
     if response.method == "POST":
+        print(response.POST)
         if response.POST.get("save"):
             for item in ls.item_set.all():
                 if response.POST.get("c" + str(item.id)) == "clicked":
                     item.complete = True
                 else:
                     item.complete = False
-                    
+
                 item.save()
         elif response.POST.get("newItem"):
             txt = response.POST.get("newTodo")
--- mysite/settings.py
+++ mysite/settings.py
@@ -115,8 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
+import os
+STATIC_URL = 'main/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
